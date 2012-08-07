2839d9e52002a05317dbca64233e27e02596f60c
Ben Peachey <potherca@gmail.com>
2012-08-07 12:07:25 +0200

    Adds first draft of PHP error finder script.

---
+++ util.d/php.find-syntax-errors-in-folder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Idea stolen from http://www.slideshare.net/nickgsuperstar/static-analysis-for-php
+
+if [ $# -lt 1 ];then
+    #@TODO: Proper usage description
+    echo 'This script expects one parameter: the directory to check for PHP (syntax) errors.'
+    exit 64
+fi
+
+DIR=$1
+
+$logFile='php-errors.log'
+
+echo '' > $logFile
+
+echo 'Will now check all files for syntax errors. This may take a while...'
+
+# @TODO: Make excluding files/folders possible (helps with vendor code and false positives)
+
+find "$DIR" -name '*.php' -exec php -l '{}' 2>&1 \; | grep -v '^No syntax errors detected in' >> $logFile
+
+cat $logFile
+
+
+# @TODO: Check file starts with either '<?' or '#!' (this includes no BOM!)
+
+#EOF
