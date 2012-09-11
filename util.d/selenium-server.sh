d1d9be9a4ae29def9ba9b2fab01e57a63ec723e7
Ben Peachey <potherca@gmail.com>
2012-09-11 13:07:29 +0200

    Adds exit status to selenium-server.sh, places log file in /var/logs and various minor improvements

--- util.d/selenium-server.sh
+++ util.d/selenium-server.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+#set -x
 
 # ==============================================================================
+# Set up some defaults
 # ------------------------------------------------------------------------------
-logFile='selenium-server.log'
+logFile='/var/log/selenium-server.log'
 screenSize='1024x768x16'
 displayNumber='22'
 portNumber='4444'
+SeleniumServer='/home/ben/Dropbox/www/3rd-party/selenium-server-standalone-2.25.0.jar'
 # ==============================================================================
 
 # ==============================================================================
@@ -15,13 +18,16 @@ function stopXvfbServer() {
 	displayNumber="$1"
 
     PID=`ps -eo pid,args  | grep "Xvfb.*:$displayNumber" | grep -v grep | awk '{ print $1 }'`
-    echo 'Stopping the Xvfb Server for display $displayNumber...'
+    echo "Stopping the Xvfb Server for display $displayNumber..."
     if [ "$PID" = "" ];then
             echo 'The Xvfb  Server was not running'
+	        RESULT=0
     else
             kill "$PID"
+            RESULT=$?
             echo 'Xvfb Server stopped'
     fi
+    return $RESULT
 }
 # ==============================================================================
 
@@ -36,11 +42,15 @@ function startXvfbServer() {
 
     if [ "$PID" = "" ];then
 	    echo "Starting the Xvfb Server on display $displayNumber..."
-	    Xvfb -fp /usr/share/fonts/X11/misc/ :$displayNumber -screen 0 $screenSize 2>&1 &
+	    Xvfb -fp /usr/share/fonts/X11/misc/ :$displayNumber -screen 0 $screenSize >> $logFile 2>&1 &
+	    RESULT=$?
 	    export DISPLAY=:$displayNumber
     else
         echo  "There is already an Xvfb Server running on display $displayNumber"
+	    RESULT=0
     fi
+    
+    return $RESULT
 }
 # ==============================================================================
 
@@ -50,33 +60,44 @@ function startXvfbServer() {
 # ------------------------------------------------------------------------------
 function startSeleniumServer() {
     startXvfbServer $displayNumber $screenSize
+    RESULT=$?
 
-    PID=`ps -eo pid,args | grep "selenium.*-port $portNumber" | grep -v grep | awk '{ print $1 }'`
-
-    if [ "$PID" = "" ];then
-        # Remove last log
-	    if [ -f "$logFile.previous" ];then
-		    rm "$logFile.previous"
-	    fi
-
-        # Backup current log
-	    if [ -f "$logFile" ];then
-		    mv "$logFile" "$logFile.previous"
-	    fi
-
-	    # Start a new log
-	    echo '' > $logFile
-
-	    # Make sure the logs can be read by other users (over ssh for instance...)
-	    # Does not work if in protected directory, so useless for now
-	    #chmod 777 $logFile
-
-	    # start the selenium server
-	    echo 'Starting the Standalone Selenium Server...'
-	    java -jar selenium-server-standalone-2.21.0.jar -port $portNumber >> $logFile 2>&1 &
+    if [ "$RESULT" != "0" ];then
+        echo 'ERROR: Can not start Selenium as the XvfbServer would not start'
+        RESULT=1
     else
-	    echo 'The Selenium Server is already running'
+
+        PID=`ps -eo pid,args | grep "selenium.*-port $portNumber" | grep -v grep | awk '{ print $1 }'`
+
+        if [ "$PID" = "" ];then
+            # Remove last log
+	        if [ -f "$logFile.previous" ];then
+		        rm "$logFile.previous"
+	        fi
+
+            # Backup current log
+	        if [ -f "$logFile" ];then
+		        mv "$logFile" "$logFile.previous"
+	        fi
+
+	        # Start a new log
+	        echo '' > $logFile
+
+	        # Make sure the logs can be read by other users (over ssh for instance...)
+	        # Does not work if in protected directory, so useless for now
+	        #chmod 777 $logFile
+
+	        # start the selenium server
+	        echo 'Starting the Standalone Selenium Server...'
+	        java -jar "$SeleniumServer" -port $portNumber >> "$logFile" 2>&1 &
+	        RESULT=$?
+        else
+	        echo 'The Selenium Server is already running'
+	        RESULT=0
+        fi
     fi
+        
+    return $RESULT
 }
 # ==============================================================================
 
@@ -90,12 +111,29 @@ function stopSeleniumServer() {
     echo 'Stopping the Selenium Server...'
     if [ "$PID" = "" ];then
 	    echo 'The Selenium Server was not running'
+        RESULT=0
     else
 	    kill "$PID"
-	    echo 'Selenium Server stopped'
+        RESULT=$?
+        
+        if [ "$RESULT" != "0" ];then
+            echo 'ERROR: Can not stop Selenium Server'
+            RESULT=1
+        else
+    	    echo 'Selenium Server stopped'
+                RESULT=0
+        fi
     fi
 
     stopXvfbServer $screenSize
+    XvfbRESULT=$?
+    
+    if [ "$XvfbRESULT" != "0" ];then
+        echo 'ERROR: Can not stop the XvfbServer'
+        XvfbRESULT=1
+    fi
+    
+    return $(expr $RESULT + $XvfbRESULT);
 }
 # ==============================================================================
 
@@ -103,8 +141,13 @@ function stopSeleniumServer() {
 # ==============================================================================
 function restartSeleniumServer() {
 # ------------------------------------------------------------------------------
-        stopSeleniumServer
-        startSeleniumServer
+    stopSeleniumServer
+    StopResult=$?
+    
+    startSeleniumServer
+    StartResult=$?
+    
+    return $(expr $StopResult + $StartResult);
 }
 # ==============================================================================
 
@@ -115,20 +158,34 @@ function run() {
     case $1 in
         start)
             startSeleniumServer
+            RESULT=$?
            ;;
         stop)
             stopSeleniumServer
+            RESULT=$?
            ;;
         restart)
             restartSeleniumServer
+            RESULT=$?
            ;;
         *)
            echo 'Please select "start", "stop" or "restart"'
            ;;
     esac
+    
+    return $RESULT
 }
 # ==============================================================================
 
-run $1
+if [ `whoami` != "root" ];then
+    echo "ERROR: Only allowed as root user"
+    RESULT=1
+else
+    run $1
+    RESULT=$?
+    tail -f $logFile &
+fi
+
+exit $RESULT
 
 #EOF
