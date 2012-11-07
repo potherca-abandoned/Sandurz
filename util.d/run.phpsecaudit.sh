92261fb39a3d3f57c8c516e4ac5eb6e865d3da2b
Ben Peachey <potherca@gmail.com>
2012-11-07 15:36:12 +0100

    Adds an output parameter to the PHPsecAudit runner.

--- util.d/run.phpsecaudit.sh
+++ util.d/run.phpsecaudit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ $# != 1 ];then
-    echo 'ERROR : This script expects exactly one parameter:  the directory to audit'
+if [ $# != 2 ];then
+    echo 'ERROR : This script expects two parameter:'
+    echo ' -- the directory to audit'
+    echo ' -- the directory to write the output to'
     exit 65
 fi
 
-time php -d error_reporting=0 /home/ben/Dropbox/www/playground/PhpSecurityAudit/phpsecaudit/run.php --src "$1"
+time php -d error_reporting=0 /home/ben/Dropbox/www/playground/PhpSecurityAudit/phpsecaudit/run.php --src "$1" --outdir "$2"
 
 #EOF
