f712e7d86352ed08cc2092ac9ced33f3b8b4536b
Ben Peachey <potherca@gmail.com>
2014-06-08 09:56:50 +0200

    Makes include-vendor-scripts more robust
     - Improves the code that tries to find the original directory the sourced script is in.

--- bashrc.d/include-vendor-scripts.sh
+++ bashrc.d/include-vendor-scripts.sh
@@ -4,10 +4,27 @@
 # ------------------------------------------------------------------------------
 
 # find out where we are located, following symlinks as the install script symlinks the `.bashrc.d` directory to `~/`
-sVendorDir="$(dirname "$(readlink -f "$0")")/../vendor"
+if [ -n "${BASH_SOURCE}" ]; then
+    sScriptPath="${BASH_SOURCE}"
+elif [ -n "$ZSH_VERSION" ]; then
+    setopt function_argzero
+    sScriptPath="${0}"
+elif eval '[[ -n ${.sh.file} ]]' 2>/dev/null; then
+    eval 'sScriptPath=${.sh.file}'
+else
+    sOpenFile="$(lsof -F n -p $$ | sed -n '$s/^n//p')"
+    if [ -n "$sOpenFile" ]; then
+        sScriptPath="${sOpenFile}"
+    else
+        echo 1>&2 "Could not reliably determine script location. Aborting"
+        exit 64
+    fi
+fi
+
+sVendorDir="$(dirname ${sScriptPath})/../vendor"
 
 aVendorScript[0]='symfony2-autocomplete/symfony2-autocomplete.bash'
-aVendorScript[0]='git-flow-completion/git-flow-completion.bash'
+aVendorScript[1]='git-flow-completion/git-flow-completion.bash'
 
 for sFile in "${aVendorScript[@]}" 
 do
