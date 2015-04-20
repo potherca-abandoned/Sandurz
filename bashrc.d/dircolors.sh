8f6bcc756099c7a14e355785405c8d396cb17bf5
Ben Peachey <potherca@gmail.com>
2015-04-20 18:35:54 +0200

    Adds check to validate dircolors exists before calling it.

--- bashrc.d/dircolors.sh
+++ bashrc.d/dircolors.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-eval `dircolors ~/.dircolors`
+if [ "$(dircolors --version > /dev/null || echo 1)" == "1" ];then
+    echo 'dircolors is not installed'
+    # @TODO: Should the user be notified of the fact that dircolors is available
+    # through coreutils which can be installed on mac using brew, etc. ?
+else
+    eval `dircolors ~/.dircolors`
+fi
 
 #EOF
