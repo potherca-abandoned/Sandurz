3f39200e4498935a68453c740261612c1de838a2
Ben Peachey <potherca@gmail.com>
2015-04-22 20:42:33 +0200

    Adds more robust logic to find common functions file.

--- functions/common.sh
+++ functions/common.sh
@@ -6,7 +6,24 @@ function sourceFunction() {
 
     # Find out where we are located, following symlinks as the install script
     # symlinks the `common.sh` file to `~/.common.sh`
-    sScriptPath=$(readlink "${HOME}/.common.sh")
+    if [ -f "${HOME}/.common.sh" ];then
+        sScriptPath=$(readlink "${HOME}/.common.sh")
+    elif [ -n "${BASH_SOURCE}" ]; then
+        sScriptPath="${BASH_SOURCE}"
+    elif [ -n "$ZSH_VERSION" ]; then
+        setopt function_argzero
+        sScriptPath="${0}"
+    elif eval '[[ -n ${.sh.file} ]]' 2>/dev/null; then
+        eval 'sScriptPath=${.sh.file}'
+    else
+        sOpenFile="$(lsof -F n -p $$ | sed -n '$s/^n//p')"
+        if [ -n "$sOpenFile" ]; then
+            sScriptPath="${sOpenFile}"
+        else
+            echo 1>&2 "Could not reliably determine script location. Aborting"
+            exit 64
+        fi
+    fi
 
     sFunctionsDirectory="$(dirname ${sScriptPath})"
     sSourceFile="${sFunctionsDirectory}/function.${sFunction}.sh"
