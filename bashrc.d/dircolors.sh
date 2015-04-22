198d37bc0c46818d8d7d5a56144989d1e94cf664
Ben Peachey <potherca@gmail.com>
2015-04-22 02:09:38 +0200

    Adds null check to discolours loader.

--- bashrc.d/dircolors.sh
+++ bashrc.d/dircolors.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ "$(dircolors --version > /dev/null || echo 1)" == "1" ];then
+if [ "$(dircolors --version 2> /dev/null)" ];then
+    eval `dircolors ~/.dircolors`
+else
     echo 'dircolors is not installed'
     # @TODO: Should the user be notified of the fact that dircolors is available
     # through coreutils which can be installed on mac using brew, etc. ?
-else
-    eval `dircolors ~/.dircolors`
 fi
 
 #EOF
