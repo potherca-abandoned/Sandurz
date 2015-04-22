041887b965fbefacf5ac7521c694ea8447d950d8
Ben Peachey <potherca@gmail.com>
2015-04-22 02:09:06 +0200

    Improves the bash_completion logic, fixes bug adds messaging.

--- bashrc.d/prompt-enhancements/bash_completion.sh
+++ bashrc.d/prompt-enhancements/bash_completion.sh
@@ -1,8 +1,21 @@
+#!/usr/bin/env bash
+
+source "${HOME}/.common.sh"
+
+sourceFunction printStatus
 
 if [ -d /etc/bash_completion ];then
     source /etc/bash_completion && echo 'Bash completion enabled'
 elif [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion && echo 'Bash completion enabled'
+    # There's an unbound variable in the bash_completion script called
+    # BASH_COMPLETION_DEBUG so we need to temporarily disable errexit
+    set +u
+    . $(brew --prefix)/etc/bash_completion && echo 'Bash completion enabled'
+    set -u
 else
-    echo ' !! WARNING: Bash completion is not installed'
+    sourceFunction printWarning
+
+    printWarning 'Bash completion is not installed'
 fi
+
+#EOF
