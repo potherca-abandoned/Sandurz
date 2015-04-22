dee39cd0c6a3ebde3da984a7dca023cb7f33126a
Ben Peachey <potherca@gmail.com>
2015-04-22 02:12:57 +0200

    Code cleanup for the installer
     - Splits logic into separate functions
     - Replaces messaging with common messaging logic
     - Adds symlink to common.sh function loader

--- install.sh
+++ install.sh
@@ -5,81 +5,115 @@ set -o errexit # exit on all and any errors, same as -e
 
 # ==============================================================================
 # ------------------------------------------------------------------------------
-DIR="$( cd "$( dirname "$0" )" && pwd )"    # Current script directory
+sScriptDirectory="$( cd "$( dirname "$0" )" && pwd )" # Current script directory
 DEBUG=1
 # ==============================================================================
 
 
 # ==============================================================================
+# Import Messaging utilities
 # ------------------------------------------------------------------------------
-function indent() {
-    c='s/^/       /'
-    case $(uname) in
-        Darwin) sed -l "$c";;
-        *)      sed -u "$c";;
-    esac
-}
-function topic() {
-    echo
-    echo "=====> $*"
-}
+source "${sScriptDirectory}/functions/common.sh"
 
-function status() {
-    echo "-----> $*"
-}
+sourceFunction indent
+sourceFunction printTopic
+sourceFunction printStatus
+sourceFunction printWarning
+#sourceFunction printDebug
+# ==============================================================================
 
-function warning() {
-  echo
-  echo " !     WARNING: $*" | indent no_first_line_indent
-  echo
-}
 
-function debug() {
-    if [ "$DEBUG" != "0" ];then
-        echo "[DEBUG] $*" >&1
+# ==============================================================================
+# ------------------------------------------------------------------------------
+function symlinkCommonFunctionsFile() {
+    printTopic 'Creating symlinks for common Bash utility functions script'
+
+    #@TODO: ask the user for input if this is correct...
+
+    ln -s -i "$sScriptDirectory/functions/common.sh" "$HOME/.common.sh"|| true
+}
+# ------------------------------------------------------------------------------
+
+
+# ==============================================================================
+# ------------------------------------------------------------------------------
+function symlinkBashFiles() {
+    printTopic 'Creating symlinks for Bash enhancements'
+
+    #@TODO: ask the user for input if this is correct...
+
+    ln -s -i "$sScriptDirectory/bash_aliases" "$HOME/.bash_aliases" \
+        && ln -s -i "$sScriptDirectory/bashrc.d" "$HOME/.bashrc.d"
+}
+# ==============================================================================
+
+
+# ==============================================================================
+# ------------------------------------------------------------------------------
+function symlinkGitFiles() {
+    printTopic 'Creating symlinks for .git files'
+
+    #@TODO: ask the user for input if this is correct...
+
+    ln -s -i "$sScriptDirectory/git.d" "$HOME/.git.d" \
+        && ln -s -i "$HOME/.git.d/config" "$HOME/.gitconfig"
+}
+# ==============================================================================
+
+
+# ==============================================================================
+# ------------------------------------------------------------------------------
+function symlinkDircolors() {
+    printTopic 'Creating symlinks for dircolor files'
+
+    # ------------------------------------------------------------------------------
+    # dircolors is available through coreutils which can be installed with brew
+    # @TODO: Prompt the user to install coreutils if not available
+    # ------------------------------------------------------------------------------
+
+    sTheme="${sScriptDirectory}/vendor/dircolors/dircolors.ansi-light"
+
+    #@TODO: ask the user for input if this is correct...
+    ln -s -i "${sTheme}" "$HOME/.dircolors" || true
+}
+# ==============================================================================
+
+
+# ==============================================================================
+# ------------------------------------------------------------------------------
+function validateBashCompletion() {
+    printTopic 'Validating bash-completion is installed'
+
+    if [ -d /etc/bash_completion ] || [ -d /usr/local/etc/bash_completion.d ];then
+        printStatus 'Bash completion is already installed'
+    else
+        printStatus 'Bash completion is not installed'
+        if [ "$(brew --version > /dev/null && echo 1)" == "1" ];then
+            printStatus "Using brew to install bash-completion"
+            brew install bash-completion
+        elif [ "$(apt-get --version > /dev/null && echo 1)" == "1" ];then
+            printStatus 'Using apt-get to install bash-completion'
+            sudo apt-get install bash-completion
+        else
+            printWarning 'Could not install bash-completion. Please install manually.'
+        fi
     fi
 }
 # ==============================================================================
 
+
 # ==============================================================================
+# Make sure that .bash_aliases is included from .profile
 # ------------------------------------------------------------------------------
-function runInstall() {
+function validateBashAliases() {
+    printTopic 'Validating .bash_aliases is included from .profile'
 
-    # ------------------------------------------------------------------------------
-    topic 'Creating symlinks for .profile, .dircolors and .git files'
-    echo "from $DIR" | indent
-    echo "into $HOME" | indent
-
-    #@TODO: ask the user for input if this is correct...
-
-    ln -s -i "$DIR/bash_aliases" "$HOME/.bash_aliases" && ln -s -i "$DIR/bashrc.d" "$HOME/.bashrc.d";
-    ln -s -i "$DIR/git.d" "$HOME/.git.d" && ln -s -i "$HOME/.git.d/config" "$HOME/.gitconfig"
-    ln -s -i "$DIR/vendor/dircolors/dircolors.ansi-light" "$HOME/.dircolors" || echo -n ''
-    # ------------------------------------------------------------------------------
-
-    # ------------------------------------------------------------------------------
-    # dircolors is available through coreutils which can be installed with brew
-    # @TODO: Prompt the user to install coreutils
-    # ------------------------------------------------------------------------------
-    # @TODO: Check bash version and (espacially when on mac) install (newer) GNU version!
-    # ------------------------------------------------------------------------------
-
-    # ------------------------------------------------------------------------------
-    #@TODO: use https://github.com/felipec/git instead
-    #ln -s -i "$DIR/vendor/git-remote-hg/git-remote-hg" "/usr/local/bin/" && sudo chmod +x '/usr/local/bin/git-remote-hg'
-    # ------------------------------------------------------------------------------
-
-    # ------------------------------------------------------------------------------
-    # Make sure that .bash_aliases is included from .profile
-    # ------------------------------------------------------------------------------
-    topic 'Validating .bash_aliases is included from .profile'
-
-    sProfilePath="$HOME/.profile"
+    sProfilePath="${HOME}/.profile"
     if [ -f "${sProfilePath}" ]; then
-        status "Found .profile in the home directory"
+        printStatus "Found .profile in the home directory"
     else
-        status "Did not find a .profile file in the home directory."
-        status "Creating .profile file in ${HOME}\n"
+        printStatus "Did not find a .profile file in the home directory."
+        printStatus "Creating .profile file in ${HOME}\n"
 
         echo '' > "${sProfilePath}"
     fi
@@ -87,43 +121,56 @@ function runInstall() {
     sResult="$(grep bash_aliases $sProfilePath || echo 1)"
 
     if [ "${sResult}" = "1" ];then
-        status "Did not find a reference to .bash_aliases in .profile"
-        status "Appending reference of .bash_aliases to .profile"
+        printStatus "Did not find a reference to .bash_aliases in .profile"
+        printStatus "Appending reference of .bash_aliases to .profile"
 
         echo -e "\n# Include .bash_aliases\nif [ -f ".bash_aliases" ]; then\n\t. .bash_aliases\nfi" >> $HOME/.profile
     else
-        status "Found reference to .bash_aliases in .profile"
+        printStatus "Found reference to .bash_aliases in .profile"
     fi
+}
+# ------------------------------------------------------------------------------
+
+
+# ==============================================================================
+# ------------------------------------------------------------------------------
+function sourceFiles() {
+    printTopic "Calling $sProfilePath for inclusion"
+    source "${sProfilePath}" | indent
+}
+# ------------------------------------------------------------------------------
+
+
+# ==============================================================================
+# ------------------------------------------------------------------------------
+function runInstall() {
+
+    echo 'Installing files'
+    echo "from $sScriptDirectory" | indent
+    echo "into $HOME" | indent
+
+    symlinkCommonFunctionsFile
+    # ------------------------------------------------------------------------------
+    # @TODO: Check bash version and (espacially when on mac) install (newer) GNU version!
     # ------------------------------------------------------------------------------
 
     # ------------------------------------------------------------------------------
-    # ------------------------------------------------------------------------------
-    topic 'Validating bash-completion is installed'
-
-    if [ -d /etc/bash_completion ] || [ -d /usr/local/etc/bash_completion.d ];then
-        status 'Bash completion is already installed'
-    else
-        status 'Bash completion is not installed'
-        if [ "$(brew --version > /dev/null && echo 1)" == "1" ];then
-            status "Using brew to install bash-completion"
-            brew install bash-completion
-        elif [ "$(apt-get --version > /dev/null && echo 1)" == "1" ];then
-            status 'Using apt-get to install bash-completion'
-            sudo apt-get install bash-completion
-        else
-            warning 'Could not install bash-completion. Please install manually.'
-        fi
-    fi
+    #@TODO: use https://github.com/felipec/git instead
+    #ln -s -i "$sScriptDirectory/vendor/git-remote-hg/git-remote-hg" "/usr/local/bin/" && sudo chmod +x '/usr/local/bin/git-remote-hg'
     # ------------------------------------------------------------------------------
 
+    symlinkBashFiles
+    symlinkGitFiles
+    symlinkDircolors
 
-    # ------------------------------------------------------------------------------
-    topic "Calling $sProfilePath for inclusion"
-    source "${sProfilePath}"
-    # ------------------------------------------------------------------------------
+    validateBashAliases
+    validateBashCompletion
+
+    sourceFiles
 }
 # ==============================================================================
 
 runInstall
+echo 'Done.'
 
 #EOF
