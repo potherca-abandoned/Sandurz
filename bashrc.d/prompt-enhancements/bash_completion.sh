1b188df391c309410b34a93bf6196aa178427a57
Ben Peachey <potherca@gmail.com>
2015-04-23 23:25:08 +0200

    Adds fix for bash_completion script showing error when brew not installed.

--- bashrc.d/prompt-enhancements/bash_completion.sh
+++ bashrc.d/prompt-enhancements/bash_completion.sh
@@ -4,9 +4,9 @@ source "${HOME}/.common.sh"
 
 sourceFunction printStatus
 
-if [ -d /etc/bash_completion ];then
+if [ -f /etc/bash_completion ];then
     source /etc/bash_completion && echo 'Bash completion enabled' | indent
-elif [ -f $(brew --prefix)/etc/bash_completion ]; then
+elif [ "$(brew --version > /dev/null 2>&1 && echo 1)" == "1" ] && [ -f $(brew --prefix)/etc/bash_completion ]; then
     # There's an unbound variable in the bash_completion script called
     # BASH_COMPLETION_DEBUG so we need to temporarily disable errexit
     set +u
