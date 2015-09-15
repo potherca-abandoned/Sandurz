d69f9791a876b09ec7403b4584f7932e38d40721
Ben Peachey <potherca@gmail.com>
2015-09-15 11:04:36 +0200

    Adds proper fix for the 'unbound variable' bug caused by bash_completion
     Replaces workaround.

--- bashrc.d/prompt-enhancements/bash_completion.sh
+++ bashrc.d/prompt-enhancements/bash_completion.sh
@@ -5,14 +5,13 @@ source "${HOME}/.common.sh"
 sourceFunction indent
 sourceFunction printStatus
 
-if [ -f /etc/bash_completion ];then
-    source /etc/bash_completion && printStatus 'Bash completion enabled' | indent
-elif [ "$(brew --version > /dev/null 2>&1 && echo 1)" == "1" ] && [ -f $(brew --prefix)/etc/bash_completion ]; then
+if [ "$(brew --version > /dev/null 2>&1 && echo 1)" == "1" ] && [ -f $(brew --prefix)/etc/bash_completion ]; then
     # There's an unbound variable in the bash_completion script called
-    # BASH_COMPLETION_DEBUG so we need to temporarily disable errexit
-    set +u
-    . $(brew --prefix)/etc/bash_completion && printStatus 'Bash completion enabled' | indent
-    set -u
+    # BASH_COMPLETION_DEBUG which needs to be set to avoid errexit
+    export BASH_COMPLETION_DEBUG=''
+    source $(brew --prefix)/etc/bash_completion && printStatus 'Bash completion enabled (brew)' | indent
+elif [ -f '/etc/bash_completion' ];then
+    source '/etc/bash_completion' && printStatus 'Bash completion enabled' | indent
 else
     sourceFunction printWarning
 
