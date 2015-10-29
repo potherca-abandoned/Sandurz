98ecc899a08cc1fdd944f87ff4f0b453287ed04b
Ben Peachey <potherca@gmail.com>
2015-10-29 13:52:52 +0100

    Adds alternative color when dircolors is not available.

--- bashrc.d/dircolors.sh
+++ bashrc.d/dircolors.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
+source "${HOME}/.common.sh"
+
 if [ "$(dircolors --version 2> /dev/null)" ];then
-    eval `dircolors ~/.dircolors`
+    eval $(dircolors ~/.dircolors)
 else
-    echo 'dircolors is not installed' | indent
     # @TODO: Should the user be notified of the fact that dircolors is available
     # through coreutils which can be installed on mac using brew, etc. ?
+    # Or should this (also) be part of the install script?
+
+    sourceFunction printWarning
+    sourceFunction isMacOS
+
+    printWarning 'dircolors is not installed'
+
+    if [ isMacOS ];then
+        sourceFunction printStatus
+
+        printStatus 'Using LSCOLORS fallback for OSX' | indent
+
+        # @see: https://github.com/seebi/dircolors-solarized/issues/10
+        export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
+    fi
 fi
 
 #EOF
