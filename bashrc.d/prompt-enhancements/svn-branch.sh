4148ed1f5788f64a7b2436e8ad54e90eff79b972
Ben Peachey <potherca@gmail.com>
2015-10-31 12:43:40 +0100

    Minor improvement for svn-branch.sh.

--- bashrc.d/prompt-enhancements/svn-branch.sh
+++ bashrc.d/prompt-enhancements/svn-branch.sh
@@ -1,18 +1,20 @@
+#!/usr/bin/env bash
 # ==============================================================================
 # Enhance prompt when inside SVN working directories
-# 
-# Inside SVN working directories this will check for updates after each time you 
-# execute a command. If the response from the server is slow, this can cause 
-# latency in the prompt cursor appearing. You can disable this by calling the 
+#
+# Inside SVN working directories this will check for updates after each time you
+# execute a command. If the response from the server is slow, this can cause
+# latency in the prompt cursor appearing. You can disable this by calling the
 # following command from the commandline:
 #
 # 	toggleSvnPromptDisplayUpdate off
 #
 # You might also want to change the default setting below...
 # ------------------------------------------------------------------------------
-if [  -n "$CHECK_REMOTE_SVN" ]; then
+if [ -z "${CHECK_REMOTE_SVN-}" ]; then
 	CHECK_REMOTE_SVN=1
 fi
+
 # ------------------------------------------------------------------------------
 
 function toggleSvnPromptDisplayUpdate {
@@ -27,15 +29,15 @@ function toggleSvnPromptDisplayUpdate {
 function get_svn_revision {
 
 	LOCAL_REVISION=$(svn info  2> /dev/null | grep Revision | cut -c11-)
-	
+
 	if [ -n "$LOCAL_REVISION" ]; then
 		if [ "$CHECK_REMOTE_SVN" = "1" ]; then
 
-			# REMOTE_REVISION should be written and read from a file stuffed in 
-			# the .svn folder and only be refreshed at set intervals (we could 
-			# stat the file for a last-checked-date). Users can still use the 
+			# REMOTE_REVISION should be written and read from a file stuffed in
+			# the .svn folder and only be refreshed at set intervals (we could
+			# stat the file for a last-checked-date). Users can still use the
 			# toggle, but it would save a whole lot of latency
-	
+
 			REMOTE_REVISION=$(svn info -r HEAD | grep -i "Revision" | cut -c11-)
 
 			if [ "$LOCAL_REVISION" = "$REMOTE_REVISION"  ]; then
@@ -60,7 +62,7 @@ function parse_svn_branch() {
 }
 
 # ------------------------------------------------------------------------------
-PS1="$PS1\$(parse_svn_branch)\$(get_svn_revision)"
+#PS1="$PS1\$(parse_svn_branch)\$(get_svn_revision)"
 # ==============================================================================
 
 #EOF
