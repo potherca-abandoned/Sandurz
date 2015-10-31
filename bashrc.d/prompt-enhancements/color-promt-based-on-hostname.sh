cc5f11d1771e46c6cd9c3b461e4187218c0446a9
Ben Peachey <potherca@gmail.com>
2015-10-31 12:42:11 +0100

    Variable naming convention fix.

--- bashrc.d/prompt-enhancements/color-promt-based-on-hostname.sh
+++ bashrc.d/prompt-enhancements/color-promt-based-on-hostname.sh
@@ -1,35 +1,37 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # ==============================================================================
-# Color the promt depending on our environment
+# Replaces the Prompt with a host name coloured depending on the environment
 # ------------------------------------------------------------------------------
-function colorPromt {
-    hostname=`uname -n`
-    
-    case "$hostname" in
+function colorPromptBasedOnHostname() {
+    local sColor
+    local sHostName=`uname -n`
+
+    case "$sHostName" in
         *-vre-*)
             # should be a PRODCUTION machine, Color the promt red
-            COLOR=41
-        ;; 
+            sColor=41
+        ;;
 
         *dev*)
             # should be a DEV machine, Color the prompt yellow
-            COLOR=43
-        ;; 
-        
-        *ben*) 
+            sColor=43
+        ;;
+
+        *ben*)
             # should be my LOCAL machine, Color the prompt green
-            COLOR=42
-        ;; 
-        
+            sColor=42
+        ;;
+
         *)
             # We're no longer in Kansas, Dorothy! Color the promt blue
-            COLOR=44
-        ;; 
+            sColor=44
+        ;;
     esac
 
-    PS1="\e[$COLOR;37m$PS1\e[m"
+    echo -en "\033[${sColor}m$(whoami)@${sHostName}\033[00m"
 }
-colorPromt
+# ------------------------------------------------------------------------------
+#PROMPT_COMMAND=colorPrompt
 # ==============================================================================
 
