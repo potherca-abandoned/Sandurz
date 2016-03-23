089355081d86885fe8b07fad1e85e9d06433f658
Ben Peachey <potherca@gmail.com>
2016-03-23 20:58:52 +0100

    Fixes the `playground` alias.

--- bashrc/aliases/playground.sh
+++ bashrc/aliases/playground.sh
@@ -1,6 +1,12 @@
+
 # ==============================================================================
-# Aliasses for popular destinations
+# Alias for my most popular destination
 # ------------------------------------------------------------------------------
-alias playground="cd ~/Dropbox/www/Playground/$1"
+alias playground='function cdToPlayground() { cd "$HOME/Dropbox/www/playground/${1:-}"; result=$?; unset -f cdToPlayground; return $result; }; cdToPlayground'
+# ------------------------------------------------------------------------------
+# Add completions for directories in the playground
+# For more details see: https://www.gnu.org/software/bash/manual/html_node/Programmable-Completion-Builtins.html
+complete -o dirnames -W "$(pushd $HOME/Dropbox/www/playground;\ls -d */;popd)" playground
 # ==============================================================================
 
+#EOF
